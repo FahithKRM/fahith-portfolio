d704028192f32eae5e0292ae25b7c071b8ffdba7
FahithKRM <krmfahith@gmail.com>
2025-12-14 19:22:28 +0530

    Added Footer

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,17 @@
+// app/components/Footer.js
+"use client";
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-white py-8">
+      <div className="container-custom px-4">
+        <div className="text-center">
+          <p className="flex items-center justify-center gap-2 text-gray-300">
+            &copy; 2024 Fahith KRM. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
